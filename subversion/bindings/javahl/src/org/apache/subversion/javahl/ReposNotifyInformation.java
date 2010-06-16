svn2github/subversion

commit 085cee46ad879b379fc93bec2d2023c5735f70e1

JavaHL: Propogate the repository notification system through to JavaHL.  This change only introduces the system for verify(), and only adds the notify actions and values that are needed by verify().  Further improvements are forthcoming.
[ in subversion/bindings/javahl/ ]
* tests/org/apache/subversion/javahl/SVNAdminTests.java
  (testVerify): New.

* native/ReposNotifyCallback.cpp,
  native/ReposNotifyCallback.h:
  New.

* native/CreateJ.h,
  native/CreateJ.cpp
  (ReposNotifyInformation): New.

* native/SVNAdmin.h,
  native/SVNAdmin.cpp
  (verify): Update to use the new callback, and the newest version of the repos
    API.

* native/org_apache_subversion_javahl_SVNAdmin.cpp
  (Java_org_apache_subversion_javahl_SVNAdmin_verify): Handle the new notifier.

* native/EnumMapper.h,
  native/EnumMapper.cpp
  (mapReposNotifyAction): New.

* src/org/apache/subversion/javahl/ISVNAdmin.java,
  src/org/apache/subversion/javahl/SVNAdmin.java
  (verify): Remove the need for an output stream, and use the notifier instead.

* src/org/apache/subversion/javahl/callback/ReposNotifyCallback.java:
  New.

* src/org/apache/subversion/javahl/ReposNotifyInformation.java:
  New.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@954003 13f79535-47bb-0310-9956-ffa450edef68

subversion/bindings/javahl/src/org/apache/subversion/javahl/ReposNotifyInformation.java
@@ -1,0 +1,105 @@
+/**
+ * @copyright
+ * ====================================================================
+ *    Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ * ====================================================================
+ * @endcopyright
+ */
+
+package org.apache.subversion.javahl;
+
+import java.util.Map;
+import java.util.EventObject;
+
+/**
+ * The event passed to the {@link Notify2#onNotify(NotifyInformation)}
+ * API to notify {@link SVNClientInterface} of relevant events.
+ *
+ * @since 1.2
+ */
+public class ReposNotifyInformation extends EventObject
+{
+    // Update the serialVersionUID when there is a incompatible change
+    // made to this class.  See any of the following, depending upon
+    // the Java release.
+    // http://java.sun.com/j2se/1.3/docs/guide/serialization/spec/version.doc7.html
+    // http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf
+    // http://java.sun.com/j2se/1.5.0/docs/guide/serialization/spec/version.html#6678
+    // http://java.sun.com/javase/6/docs/platform/serialization/spec/version.html#6678
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The {@link NotifyAction} which triggered this event.
+     */
+    private Action action;
+
+    /**
+     * The revision of the item.
+     */
+    private long revision;
+
+    /**
+     * The warning text.
+     */
+    private String warning;
+
+    /**
+     * This constructor is to be used by the native code.
+     *
+     * @param action The {@link NotifyAction} which triggered this event.
+     * @param revision potentially the revision.
+     */
+    public ReposNotifyInformation(Action action, long revision, String warning)
+    {
+        super(action);
+        this.action = action;
+        this.revision = revision;
+        this.warning = warning;
+    }
+
+    /**
+     * @return The {@link NotifyAction} which triggered this event.
+     */
+    public Action getAction()
+    {
+        return action;
+    }
+
+    /**
+     * @return The revision for the item.
+     */
+    public long getRevision()
+    {
+        return revision;
+    }
+
+    /**
+     * The type of action triggering the notification
+     */
+    public enum Action
+    {
+        /** A warning message is waiting. */
+        warning,
+
+        /** A revision has finished being dumped. */
+        dump_rev_end,
+
+        /** A revision has finished being verified. */
+        verify_rev_end;
+    }
+}

subversion/bindings/javahl/src/org/apache/subversion/javahl/SVNAdmin.java
@@ -28,6 +28,8 @@ import java.io.OutputStream;
 import java.io.InputStream;
 import java.io.File;
 
+import org.apache.subversion.javahl.callback.ReposNotifyCallback;
+
 /**
  * This class offers the same commands as the svnadmin commandline
  * client.
@@ -240,18 +242,8 @@ public class SVNAdmin implements ISVNAdmin
                                   boolean usePostRevPropChangeHook)
             throws SubversionException;
 
-    /**
-     * Verify the repository at <code>path</code> between revisions
-     * <code>start</code> and <code>end</code>.
-     *
-     * @param path              the path to the repository
-     * @param messageOut        the receiver of all messages
-     * @param start             the first revision
-     * @param end               the last revision
-     * @throws ClientException If an error occurred.
-     */
-    public native void verify(File path, OutputStream messageOut,
-                              Revision start, Revision end)
+    public native void verify(File path, Revision start, Revision end,
+                              ReposNotifyCallback callback)
             throws ClientException;
 
     /**

subversion/bindings/javahl/native/CreateJ.cpp
@@ -691,6 +691,50 @@ CreateJ::ClientNotifyInformation(const svn_wc_notify_t *wcNotify,
 }
 
 jobject
+CreateJ::ReposNotifyInformation(const svn_repos_notify_t *reposNotify,
+                                apr_pool_t *pool)
+{
+  JNIEnv *env = JNIUtil::getEnv();
+
+  // Create a local frame for our references
+  env->PushLocalFrame(LOCAL_FRAME_SIZE);
+  if (JNIUtil::isJavaExceptionThrown())
+    return NULL;
+
+  static jmethodID midCT = 0;
+  jclass clazz = env->FindClass(JAVA_PACKAGE"/ReposNotifyInformation");
+  if (JNIUtil::isJavaExceptionThrown())
+    POP_AND_RETURN_NULL;
+
+  if (midCT == 0)
+    {
+      midCT = env->GetMethodID(clazz, "<init>",
+                               "(L"JAVA_PACKAGE"/ReposNotifyInformation$Action;"
+                               "JLjava/lang/String;)V");
+      if (JNIUtil::isJavaExceptionThrown() || midCT == 0)
+        POP_AND_RETURN_NULL;
+    }
+
+  // convert the parameters to their Java relatives
+  jobject jAction = EnumMapper::mapReposNotifyAction(reposNotify->action);
+  if (JNIUtil::isJavaExceptionThrown())
+    POP_AND_RETURN_NULL;
+
+  jstring jWarning = JNIUtil::makeJString(reposNotify->warning);
+  if (JNIUtil::isJavaExceptionThrown())
+    POP_AND_RETURN_NULL;
+
+  jlong jRevision = (jlong)reposNotify->revision;
+
+  // call the Java method
+  jobject jInfo = env->NewObject(clazz, midCT, jAction, jRevision, jWarning);
+  if (JNIUtil::isJavaExceptionThrown())
+    POP_AND_RETURN_NULL;
+
+  return env->PopLocalFrame(jInfo);
+}
+
+jobject
 CreateJ::RevisionRangeList(apr_array_header_t *ranges)
 {
   JNIEnv *env = JNIUtil::getEnv();

subversion/bindings/javahl/native/CreateJ.h
@@ -29,6 +29,7 @@
 
 #include <jni.h>
 #include "svn_wc.h"
+#include "svn_repos.h"
 #include "svn_client.h"
 
 #include <vector>
@@ -56,6 +57,9 @@ class CreateJ
 
   static jobject
   ClientNotifyInformation(const svn_wc_notify_t *notify, apr_pool_t *pool);
+
+  static jobject
+  ReposNotifyInformation(const svn_repos_notify_t *notify, apr_pool_t *pool);
 
   static jobject
   RevisionRangeList(apr_array_header_t *ranges);

subversion/bindings/javahl/native/EnumMapper.cpp
@@ -77,6 +77,15 @@ jobject EnumMapper::mapNotifyAction(svn_wc_notify_action_t action)
 }
 
 /**
+ * Map a C repos notify action constant to the Java constant.
+ */
+jobject EnumMapper::mapReposNotifyAction(svn_repos_notify_action_t action)
+{
+  // We're assuming a valid value for the C enum above
+  return mapEnum(JAVA_PACKAGE"/ReposNotifyInformation$Action", (int) action);
+}
+
+/**
  * Map a C node kind constant to the Java constant.
  */
 jobject EnumMapper::mapNodeKind(svn_node_kind_t nodeKind)

subversion/bindings/javahl/native/EnumMapper.h
@@ -30,6 +30,7 @@
 #include <jni.h>
 #include "svn_client.h"
 #include "svn_wc.h"
+#include "svn_repos.h"
 #include "svn_types.h"
 
 class JNIStringHolder;
@@ -52,6 +53,7 @@ class EnumMapper
   static jint mapCommitMessageStateFlags(apr_byte_t flags);
   static jobject mapNotifyState(svn_wc_notify_state_t state);
   static jobject mapNotifyAction(svn_wc_notify_action_t action);
+  static jobject mapReposNotifyAction(svn_repos_notify_action_t action);
   static jobject mapNodeKind(svn_node_kind_t nodeKind);
   static jobject mapNotifyLockState(svn_wc_notify_lock_state_t state);
   static jobject mapStatusKind(svn_wc_status_kind svnKind);

subversion/bindings/javahl/native/ReposNotifyCallback.cpp
@@ -1,0 +1,93 @@
+/**
+ * @copyright
+ * ====================================================================
+ *    Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ * ====================================================================
+ * @endcopyright
+ *
+ * @file ReposNotifyCallback.cpp
+ * @brief Implementation of the class ReposNotifyCallback
+ */
+
+#include "ReposNotifyCallback.h"
+#include "JNIUtil.h"
+#include "CreateJ.h"
+#include "EnumMapper.h"
+#include "RevisionRange.h"
+
+/**
+ * Create a new object and store the Java object.
+ * @param notify    global reference to the Java object
+ */
+ReposNotifyCallback::ReposNotifyCallback(jobject p_notify)
+{
+  m_notify = p_notify;
+}
+
+ReposNotifyCallback::~ReposNotifyCallback()
+{
+  // Don't need to destroy the reference, since it was given us by Java
+}
+
+void
+ReposNotifyCallback::notify(void *baton, const svn_repos_notify_t *notify,
+                            apr_pool_t *pool)
+{
+  if (baton)
+    ((ReposNotifyCallback *)baton)->onNotify(notify, pool);
+}
+
+/**
+ * Handler for Subversion notifications.
+ *
+ * @param notify all the information about the event
+ * @param pool an apr pool to allocated memory
+ */
+void
+ReposNotifyCallback::onNotify(const svn_repos_notify_t *wcNotify,
+                              apr_pool_t *pool)
+{
+  JNIEnv *env = JNIUtil::getEnv();
+
+  // Java method id will not change during the time this library is
+  // loaded, so it can be cached.
+  static jmethodID mid = 0;
+  if (mid == 0)
+    {
+      jclass clazz = env->FindClass(JAVA_PACKAGE"/callback/ReposNotifyCallback");
+      if (JNIUtil::isJavaExceptionThrown())
+        return;
+
+      mid = env->GetMethodID(clazz, "onNotify",
+                             "(L"JAVA_PACKAGE"/ReposNotifyInformation;)V");
+      if (JNIUtil::isJavaExceptionThrown() || mid == 0)
+        return;
+
+      env->DeleteLocalRef(clazz);
+    }
+
+  jobject jInfo = CreateJ::ReposNotifyInformation(wcNotify, pool);
+  if (JNIUtil::isJavaExceptionThrown())
+    return;
+
+  env->CallVoidMethod(m_notify, mid, jInfo);
+  if (JNIUtil::isJavaExceptionThrown())
+    return;
+
+  env->DeleteLocalRef(jInfo);
+}

subversion/bindings/javahl/native/ReposNotifyCallback.h
@@ -1,0 +1,70 @@
+/**
+ * @copyright
+ * ====================================================================
+ *    Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ * ====================================================================
+ * @endcopyright
+ *
+ * @file ReposNotifyCallback.h
+ * @brief Interface of the class ReposNotifyCallback
+ */
+
+#ifndef REPOSNOTIFYCALLBACK_H
+#define REPOSNOTIFYCALLBACK_H
+
+#include <jni.h>
+#include "svn_repos.h"
+
+/**
+ * This class passes notification from subversion to a Java object
+ * (1.2 version).
+ */
+class ReposNotifyCallback
+{
+ private:
+  /**
+   * The local reference to the Java object.
+   */
+  jobject m_notify;
+
+ public:
+  ReposNotifyCallback(jobject p_notify);
+  ~ReposNotifyCallback();
+
+  /**
+   * Implementation of the svn_repos_notify_func_t API.
+   *
+   * @param baton notification instance is passed using this parameter
+   * @param notify all the information about the event
+   * @param pool An APR pool from which to allocate memory.
+   */
+  static void notify(void *baton,
+                     const svn_repos_notify_t *notify,
+                     apr_pool_t *pool);
+
+  /**
+   * Handler for Subversion notifications.
+   *
+   * @param notify all the information about the event
+   * @param pool An APR pool from which to allocate memory.
+   */
+  void onNotify(const svn_repos_notify_t *notify,
+                apr_pool_t *pool);
+};
+
+#endif  // REPOSNOTIFYCALLBACK_H

commit 70f2e7781bbb45e2e6363c16b67d0a86d99142dc

JavaHL: Fix the compat build failures introduced by r954003 and r954009.
[ in subversion/bindings/javahl/ ]
* src/org/apache/subversion/javahl/ReposNotifyInformation.java
  (getWarning): New.

* src/org/tigris/subversion/javahl/SVNAdmin.java
  (dump, verify): Use a wrapper class to fix the API call.
  (ReposNotifyHandler): New.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@955113 13f79535-47bb-0310-9956-ffa450edef68

subversion/bindings/javahl/src/org/apache/subversion/javahl/ReposNotifyInformation.java
@@ -89,6 +89,14 @@ public class ReposNotifyInformation extends EventObject
     }
 
     /**
+     * @return The warning text.
+     */
+    public String getWarning()
+    {
+        return warning;
+    }
+
+    /**
      * The type of action triggering the notification
      */
     public enum Action

subversion/bindings/javahl/src/org/tigris/subversion/javahl/SVNAdmin.java
@@ -170,10 +170,10 @@ public class SVNAdmin
         try
         {
             aSVNAdmin.dump(new File(path), new OutputWrapper(dataOut),
-                           new OutputWrapper(errorOut),
                            start == null ? null : start.toApache(),
                            end == null ? null : end.toApache(),
-                           incremental, useDeltas);
+                           incremental, useDeltas,
+                           new ReposNotifyHandler(errorOut));
         }
         catch (org.apache.subversion.javahl.ClientException ex)
         {
@@ -436,9 +436,10 @@ public class SVNAdmin
     {
         try
         {
-            aSVNAdmin.verify(new File(path), new OutputWrapper(messageOut),
+            aSVNAdmin.verify(new File(path),
                              start == null ? null : start.toApache(),
-                             end == null ? null : end.toApache());
+                             end == null ? null : end.toApache(),
+                             new ReposNotifyHandler(messageOut));
         }
         catch (org.apache.subversion.javahl.ClientException ex)
         {
@@ -548,6 +549,51 @@ public class SVNAdmin
         public void close() throws IOException
         {
             inputer.close();
+        }
+    }
+
+    private class ReposNotifyHandler
+        implements org.apache.subversion.javahl.callback.ReposNotifyCallback
+    {
+        private OutputInterface outputer;
+
+        public ReposNotifyHandler(OutputInterface outputer)
+        {
+            this.outputer = outputer;
+        }
+
+        public void onNotify(org.apache.subversion.javahl.ReposNotifyInformation
+                                                                           info)
+        {
+            String val;
+
+            switch (info.getAction())
+            {
+                case warning:
+                    val = info.getWarning();
+                    break;
+
+                case dump_rev_end:
+                    val = "* Dumped revision " + info.getRevision() + ".\n";
+                    break;
+
+                case verify_rev_end:
+                    val = "* Verified revision " + info.getRevision() + ".\n";
+                    break;
+
+                default:
+                    val = null;
+            }
+
+            if (val != null)
+                try 
+                {
+                    outputer.write(val.getBytes());
+                }
+                catch (IOException ex)
+                {
+                    ; // ignore
+                }
         }
     }
 }