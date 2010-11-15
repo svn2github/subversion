svn2github/subversion

commit 27175b7a5014078a0bde98c366b89fde61a9b6c9

I think this will fix the "SQL statements in progress" errors, although I don't see them locally.
* subversion/libsvn_wc/wc_db.c (read_tree_conflict): Reset statement.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1035277 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/wc_db.c
@@ -4372,6 +4372,7 @@ read_tree_conflict(const svn_wc_conflict_description2_t **tree_conflict,
 
   conflict_data = svn_sqlite__column_text(stmt, 0, NULL);
   skel = svn_skel__parse(conflict_data, strlen(conflict_data), scratch_pool);
+  SVN_ERR(svn_sqlite__reset(stmt));
   SVN_ERR(svn_wc__deserialize_conflict(tree_conflict, skel,
                                        pdh->wcroot->abspath, result_pool,
                                        scratch_pool));

commit 002cbdda7343a7bd80dbdc622203bf48debfa044

* subversion/libsvn_wc/wc_db.c   (read_tree_conflict): Following up on r1035277, only reset the statement     after the skel is deserialized. This should fix many tests that started     failing after r1035277.
git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1035293 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/wc_db.c
@@ -4356,7 +4356,8 @@ read_tree_conflict(const svn_wc_conflict_description2_t **tree_conflict,
   svn_boolean_t have_row;
   const char *conflict_data;
   const svn_skel_t *skel;
-  
+  svn_error_t *err;
+
   *tree_conflict = NULL;
 
   if (!local_relpath[0])
@@ -4372,12 +4373,12 @@ read_tree_conflict(const svn_wc_conflict_description2_t **tree_conflict,
 
   conflict_data = svn_sqlite__column_text(stmt, 0, NULL);
   skel = svn_skel__parse(conflict_data, strlen(conflict_data), scratch_pool);
-  SVN_ERR(svn_sqlite__reset(stmt));
-  SVN_ERR(svn_wc__deserialize_conflict(tree_conflict, skel,
-                                       pdh->wcroot->abspath, result_pool,
-                                       scratch_pool));
+  err = svn_wc__deserialize_conflict(tree_conflict, skel,
+                                     pdh->wcroot->abspath, result_pool,
+                                     scratch_pool);
 
-  return SVN_NO_ERROR;
+  return svn_error_compose_create(err,
+                                  svn_sqlite__reset(stmt));
 }
 
 svn_error_t *