svn2github/subversion

commit 7e91ad35396dc69121ec99dde715b2a78ed41415

Move determination of switched revision status out of the node walker.
* subversion/libsvn_wc/revision_status.c
  (analyze_status): Stop caring about switched nodes.
  (svn_wc_revision_status2): Ask svn_wc__db_revision_status() for switched
   items. Move the code around a bit so things are done in more logical order.

* subversion/libsvn_wc/wc-queries.sql
  (STMT_SELECT_SWITCHED_NODES): New query, selects all switched nodes
   beneath a given local abspath. It needs to know the repos_path of
   local_abspath's wcroot to carry out its job.

* subversion/libsvn_wc/wc_db.c
  (svn_wc__db_revision_status): New output argument 'is_switched'.
   Check for swiched nodes any children of local_abspath.

* subversion/libsvn_wc/wc_db.h
  (svn_wc__db_revision_status): New output argument 'is_switched'.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1079828 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/wc-queries.sql
@@ -893,6 +893,15 @@ WHERE wc_id = ?1 AND (local_relpath = ?2 OR local_relpath LIKE ?3 ESCAPE '#')
        presence = 'base-deleted')
   AND file_external IS NULL;
 
+/* This query expects repos_path(wcroot)/% as arg 4,
+   and repos_path(wcroot) as arg 5. */
+-- STMT_SELECT_SWITCHED_NODES
+SELECT local_relpath FROM nodes_base
+WHERE wc_id = ?1 AND (local_relpath = ?2 OR local_relpath LIKE ?3 ESCAPE '#')
+  AND ((repos_path NOT LIKE ?4 ESCAPE '#' AND repos_path != local_relpath)
+       OR (repos_path != ?5 || local_relpath))
+  AND file_external IS NULL;
+
 /* Grab all the statements related to the schema.  */
 
 -- include: wc-metadata

subversion/libsvn_wc/wc_db.c
@@ -9133,12 +9133,14 @@ svn_wc__db_revision_status(svn_revnum_t *min_revision,
                            svn_revnum_t *max_revision,
                            svn_boolean_t *is_sparse_checkout,
                            svn_boolean_t *is_modified,
+                           svn_boolean_t *is_switched,
                            svn_wc__db_t *db,
                            const char *local_abspath,
                            apr_pool_t *scratch_pool)
 {
   svn_sqlite__stmt_t *stmt;
   svn_wc__db_wcroot_t *wcroot;
+  const char *wcroot_repos_relpath;
   const char *local_relpath;
   svn_boolean_t have_row;
 
@@ -9193,6 +9195,24 @@ svn_wc__db_revision_status(svn_revnum_t *min_revision,
   /* If this query returns a row, the working copy is modified. */
   SVN_ERR(svn_sqlite__step(&have_row, stmt));
   *is_modified = have_row;
+  SVN_ERR(svn_sqlite__reset(stmt));
+
+  /* Check for switched nodes. */
+  SVN_ERR(svn_wc__db_read_info(NULL, NULL, NULL, &wcroot_repos_relpath,
+                               NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                               NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                               NULL, NULL, NULL, NULL, NULL, NULL, db,
+                               wcroot->abspath, scratch_pool, scratch_pool));
+  SVN_ERR(svn_sqlite__get_statement(&stmt, wcroot->sdb,
+                                    STMT_SELECT_SWITCHED_NODES));
+  SVN_ERR(svn_sqlite__bindf(stmt, "issss", wcroot->wc_id, local_relpath,
+                            construct_like_arg(local_relpath, scratch_pool),
+                            construct_like_arg(wcroot_repos_relpath,
+                                               scratch_pool),
+                            wcroot_repos_relpath));
+  /* If this query returns a row, some part of the working copy is switched. */
+  SVN_ERR(svn_sqlite__step(&have_row, stmt));
+  *is_switched = have_row;
   SVN_ERR(svn_sqlite__reset(stmt));
 
   return SVN_NO_ERROR;

subversion/libsvn_wc/revision_status.c
@@ -81,17 +81,6 @@ analyze_status(const char *local_abspath,
   if (is_file_external)
     return SVN_NO_ERROR;
 
-  if (! wb->result->switched)
-    {
-      svn_boolean_t wc_root;
-      svn_boolean_t switched;
-
-      SVN_ERR(svn_wc__check_wc_root(&wc_root, NULL, &switched, wb->db,
-                                    local_abspath, scratch_pool));
-
-      wb->result->switched |= switched;
-    }
-
   item_rev = (wb->committed
               ? changed_rev
               : revision);
@@ -132,8 +121,8 @@ svn_wc_revision_status2(svn_wc_revision_status_t **result_p,
                         apr_pool_t *scratch_pool)
 {
   struct walk_baton wb;
-  const char *url;
   svn_wc_revision_status_t *result = apr_palloc(result_pool, sizeof(*result));
+
   *result_p = result;
 
   SVN_ERR_ASSERT(svn_dirent_is_absolute(local_abspath));
@@ -145,20 +134,22 @@ svn_wc_revision_status2(svn_wc_revision_status_t **result_p,
   result->modified = FALSE;
   result->sparse_checkout = FALSE;
 
-  /* initialize walking baton */
-  wb.result = result;
-  wb.committed = committed;
-  wb.local_abspath = local_abspath;
-  wb.db = wc_ctx->db;
+  SVN_ERR(svn_wc__db_revision_status(&result->min_rev, &result->max_rev,
+                                     &result->sparse_checkout,
+                                     &result->modified,
+                                     &result->switched,
+                                     wc_ctx->db,
+                                     local_abspath, scratch_pool));
 
-  SVN_ERR(svn_wc__internal_node_get_url(&url, wc_ctx->db, local_abspath,
-                                        result_pool, scratch_pool));
-
-  if (trail_url != NULL)
+  if (!result->switched && trail_url != NULL)
     {
+      const char *url;
+
       /* If the trailing part of the URL of the working copy directory
          does not match the given trailing URL then the whole working
          copy is switched. */
+      SVN_ERR(svn_wc__internal_node_get_url(&url, wc_ctx->db, local_abspath,
+                                            result_pool, scratch_pool));
       if (! url)
         {
           result->switched = TRUE;
@@ -172,10 +163,11 @@ svn_wc_revision_status2(svn_wc_revision_status_t **result_p,
         }
     }
 
-  SVN_ERR(svn_wc__db_revision_status(&result->min_rev, &result->max_rev,
-                                     &result->sparse_checkout,
-                                     &result->modified, wc_ctx->db,
-                                     local_abspath, scratch_pool));
+  /* initialize walking baton */
+  wb.result = result;
+  wb.committed = committed;
+  wb.local_abspath = local_abspath;
+  wb.db = wc_ctx->db;
 
   SVN_ERR(svn_wc__node_walk_children(wc_ctx,
                                      local_abspath,

subversion/libsvn_wc/wc_db.h
@@ -2511,13 +2511,15 @@ svn_wc__db_temp_below_work(svn_boolean_t *have_work,
  *
  * Indicate in *IS_SPARSE_CHECKOUT whether any of the nodes within
  * LOCAL_ABSPATH is sparse.
- *
- * Indicate in *IS_MODIFIED whether the working copy has local modifications. */
+ * Indicate in *IS_MODIFIED whether the working copy has local modifications.
+ * Indicate in *IS_SWITCHED whether any node beneath LOCAL_ABSPATH
+ * is switched. */
 svn_error_t *
 svn_wc__db_revision_status(svn_revnum_t *min_revision,
                            svn_revnum_t *max_revision,
                            svn_boolean_t *is_sparse_checkout,
                            svn_boolean_t *is_modified,
+                           svn_boolean_t *is_switched,
                            svn_wc__db_t *db,
                            const char *local_abspath,
                            apr_pool_t *scratch_pool);

commit 1add47dd17435d9807380b63f7150cc4ad10abfc

Fix a regression introduced in r1079828. Working copies not coming from the repository root were reported as switched.
* subversion/tests/cmdline/svnversion_tests.py
  (non_reposroot_wc): Remove XFail marker.

* subversion/libsvn_wc/wc-queries.sql
  (STMT_SELECT_SWITCHED_NODES): Do not check for switched status of the
   working copy root, this is done in the C code. Note that this query
   expects a slash for non-empty wcroot REPOS_PATHs.

* subversion/libsvn_wc/wc_db.c
  (svn_wc__db_revision_status): While binding arguments to the
   STMT_SELECT_SWITCHED_NODES query, append a slash to non-empty REPOS_PATHs
   of the working copy root. Allows the query to match the path correctly
   when concatenated with a LOCAL_RELPATH.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1080248 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/wc-queries.sql
@@ -895,10 +895,12 @@ WHERE wc_id = ?1 AND (local_relpath = ?2 OR local_relpath LIKE ?3 ESCAPE '#')
   AND file_external IS NULL;
 
 /* This query expects repos_path(wcroot)/% as arg 4,
-   and repos_path(wcroot) as arg 5. */
+   and repos_path(wcroot), with a slash appended unless the path is empty,
+   as arg 5. */
 -- STMT_SELECT_SWITCHED_NODES
 SELECT local_relpath FROM nodes_base
-WHERE wc_id = ?1 AND (local_relpath = ?2 OR local_relpath LIKE ?3 ESCAPE '#')
+WHERE wc_id = ?1 AND local_relpath != ""
+  AND (local_relpath = ?2 OR local_relpath LIKE ?3 ESCAPE '#')
   AND ((repos_path NOT LIKE ?4 ESCAPE '#' AND repos_path != local_relpath)
        OR (repos_path != ?5 || local_relpath))
   AND file_external IS NULL;

subversion/libsvn_wc/wc_db.c
@@ -9212,7 +9212,10 @@ svn_wc__db_revision_status(svn_revnum_t *min_revision,
                             construct_like_arg(local_relpath, scratch_pool),
                             construct_like_arg(wcroot_repos_relpath,
                                                scratch_pool),
-                            wcroot_repos_relpath));
+                            wcroot_repos_relpath[0] == '\0' ?
+                              "" : apr_pstrcat(scratch_pool,
+                                               wcroot_repos_relpath, "/",
+                                               (char *)NULL)));
   /* If this query returns a row, some part of the working copy is switched. */
   SVN_ERR(svn_sqlite__step(&have_row, stmt));
   *is_switched = have_row;

subversion/tests/cmdline/svnversion_tests.py
@@ -305,8 +305,6 @@ def committed_revisions(sbox):
                                             [ "1:2\n" ], [],
                                             "--committed")
 
-# This was probably broken by r1079828, stsp is investigating...
-@XFail()
 def non_reposroot_wc(sbox):
   "test 'svnversion' on a non-repos-root working copy"
   sbox.build(create_wc=False)