svn2github/subversion

commit 0597ee398507ce60b4f2a02fbb0d11e0129ccf20

* subversion/svn/main.c   (svn_cl__cmd_table): In the output of 'svn help update', document    the 'R' status character, and document what the various columns are    used for. Also, add the same documentation for status characters to    the output of 'svn help merge'.
git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@879176 13f79535-47bb-0310-9956-ffa450edef68

subversion/svn/main.c
@@ -623,6 +623,23 @@ const svn_opt_subcommand_desc2_t svn_cl__cmd_table[] =
      "  the sources have identical basenames that match a file within '.':\n"
      "  in which case, the differences will be applied to that file.\n"
      "\n"
+     "  For each merged item a line will be printed with characters reporting\n"
+     "  the action taken. These characters have the following meaning:\n"
+     "\n"
+     "    A  Added\n"
+     "    D  Deleted\n"
+     "    U  Updated\n"
+     "    C  Conflict\n"
+     "    G  Merged\n"
+     "    E  Existed\n"
+     "    R  Replaced\n"
+     "\n"
+     "  Characters in the first column report about the item itself.\n"
+     "  Characters in the second column report about properties of the item.\n"
+     "  A 'C' in the third column indicates a tree conflict, while a 'C' in\n"
+     "  the first and second columns indicate textual conflicts in files\n"
+     "  and in property values, respectively.\n"
+     "\n"
      "  NOTE:  Subversion will only record metadata to track the merge\n"
      "  if the two sources are on the same line of history -- if the\n"
      "  first source is an ancestor of the second, or vice-versa.  This is\n"
@@ -985,8 +1002,8 @@ const svn_opt_subcommand_desc2_t svn_cl__cmd_table[] =
      "  If no revision is given, bring working copy up-to-date with HEAD rev.\n"
      "  Else synchronize working copy to revision given by -r.\n"
      "\n"
-     "  For each updated item a line will start with a character reporting the\n"
-     "  action taken.  These characters have the following meaning:\n"
+     "  For each updated item a line will be printed with characters reporting\n"
+     "  the action taken. These characters have the following meaning:\n"
      "\n"
      "    A  Added\n"
      "    D  Deleted\n"
@@ -994,11 +1011,13 @@ const svn_opt_subcommand_desc2_t svn_cl__cmd_table[] =
      "    C  Conflict\n"
      "    G  Merged\n"
      "    E  Existed\n"
+     "    R  Replaced\n"
      "\n"
-     "  A character in the first column signifies an update to the actual file,\n"
-     "  while updates to the file's properties are shown in the second column.\n"
-     "  A 'B' in the third column signifies that the lock for the file has\n"
-     "  been broken or stolen.\n"
+     "  Characters in the first column report about the item itself.\n"
+     "  Characters in the second column report about properties of the item.\n"
+     "  A 'C' in the third column indicates a tree conflict, while a 'C' in\n"
+     "  the first and second columns indicate textual conflicts in files\n"
+     "  and in property values, respectively.\n"
      "\n"
      "  If --force is used, unversioned obstructing paths in the working\n"
      "  copy do not automatically cause a failure if the update attempts to\n"

commit 147f86ca8d2d7846b143c7eb6aac1bb6bb880082

* subversion/svn/main.c   (svn_cl__cmd_table): Fix help text for 'update' that appears to have been     broken by r879176.
git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1082700 13f79535-47bb-0310-9956-ffa450edef68

subversion/svn/main.c
@@ -1362,7 +1362,9 @@ const svn_opt_subcommand_desc2_t svn_cl__cmd_table[] =
      "\n"
      "  Characters in the first column report about the item itself.\n"
      "  Characters in the second column report about properties of the item.\n"
-     "  A 'C' in the third column indicates a tree conflict, while a 'C' in\n"
+     "  A 'B' in the third column signifies that the lock for the file has\n"
+     "  been broken or stolen.\n"
+     "  A 'C' in the fourth column indicates a tree conflict, while a 'C' in\n"
      "  the first and second columns indicate textual conflicts in files\n"
      "  and in property values, respectively.\n"
      "\n"