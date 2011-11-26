svn2github/subversion

commit 58dee25aabdb5fbe739aea2595950bc2ab1d4056

--accept: document the value "working" and the shorthands in the help text.
* subversion/svn/main.c
  (svn_cl__options.--accept): Update help text.

* subversion/tests/cmdline/getopt_tests_data/svn_help_log_switch_stdout:
    Update expected output.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1206523 13f79535-47bb-0310-9956-ffa450edef68

subversion/svn/main.c
@@ -301,11 +301,14 @@ const apr_getopt_option_t svn_cl__options[] =
   {"accept",        opt_accept, 1,
                     N_("specify automatic conflict resolution action\n"
                        "                             "
-                       "('postpone', 'base', 'mine-conflict',\n"
+                       "('postpone', 'working', 'base', 'mine-conflict',\n"
                        "                             "
                        "'theirs-conflict', 'mine-full', 'theirs-full',\n"
                        "                             "
-                       "'edit', 'launch')")},
+                       "'edit', 'launch')\n"
+                       "                             "
+                       "(shorthand: 'p', 'mc', 'tc', 'mf', 'tf', 'e', 'l')\n"
+                       )},
   {"show-revs",     opt_show_revs, 1,
                     N_("specify which collection of revisions to display\n"
                        "                             "

subversion/tests/cmdline/getopt_tests_data/svn_help_log_switch_stdout
@@ -157,9 +157,10 @@ Valid options:
   --ignore-ancestry        : ignore ancestry when calculating merges
   --force                  : force operation to run
   --accept ARG             : specify automatic conflict resolution action
-                             ('postpone', 'base', 'mine-conflict',
+                             ('postpone', 'working', 'base', 'mine-conflict',
                              'theirs-conflict', 'mine-full', 'theirs-full',
                              'edit', 'launch')
+                             (shorthand: 'p', 'mc', 'tc', 'mf', 'tf', 'e', 'l')
 
 Global options:
   --username ARG           : specify a username ARG

commit 179855e999fcbf72f0d1ec5249169aa9eb5ce48e

* subversion/svn/main.c: Fix output broken by r1206523.
git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1206533 13f79535-47bb-0310-9956-ffa450edef68

subversion/svn/main.c
@@ -307,7 +307,7 @@ const apr_getopt_option_t svn_cl__options[] =
                        "                             "
                        "'edit', 'launch')\n"
                        "                             "
-                       "(shorthand: 'p', 'mc', 'tc', 'mf', 'tf', 'e', 'l')\n"
+                       "(shorthand: 'p', 'mc', 'tc', 'mf', 'tf', 'e', 'l')"
                        )},
   {"show-revs",     opt_show_revs, 1,
                     N_("specify which collection of revisions to display\n"