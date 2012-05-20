svn2github/subversion

commit 99fea9b07978ed92d8e602afa8d3e1e740ed8296

* subversion/libsvn_wc/wc_db.c   (delete_node): Avoid a currently still expensive query for nodes where     the result is not interesting.
git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1340605 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/wc_db.c
@@ -6465,24 +6465,30 @@ delete_node(void *baton,
       || status == svn_wc__db_status_not_present)
     return SVN_NO_ERROR;
 
-  SVN_ERR(svn_sqlite__get_statement(&stmt, wcroot->sdb,
-                                    STMT_HAS_SERVER_EXCLUDED_NODES));
-  SVN_ERR(svn_sqlite__bindf(stmt, "is",
-                            wcroot->wc_id, local_relpath));
-  SVN_ERR(svn_sqlite__step(&have_row, stmt));
-  if (have_row)
+  /* Don't copy BASE directories with server excluded nodes */
+  if (status == svn_wc__db_status_normal && kind == svn_kind_dir)
     {
-      const char *absent_path = svn_sqlite__column_text(stmt, 0, scratch_pool);
+      SVN_ERR(svn_sqlite__get_statement(&stmt, wcroot->sdb,
+                                        STMT_HAS_SERVER_EXCLUDED_NODES));
+      SVN_ERR(svn_sqlite__bindf(stmt, "is",
+                                wcroot->wc_id, local_relpath));
+      SVN_ERR(svn_sqlite__step(&have_row, stmt));
+      if (have_row)
+        {
+          const char *absent_path = svn_sqlite__column_text(stmt, 0,
+                                                            scratch_pool);
 
-      return svn_error_createf(SVN_ERR_WC_PATH_UNEXPECTED_STATUS,
+          return svn_error_createf(
+                               SVN_ERR_WC_PATH_UNEXPECTED_STATUS,
                                svn_sqlite__reset(stmt),
                           _("Cannot delete '%s' as '%s' is excluded by server"),
                                path_for_error_message(wcroot, local_relpath,
                                                       scratch_pool),
                                path_for_error_message(wcroot, absent_path,
                                                       scratch_pool));
+        }
+      SVN_ERR(svn_sqlite__reset(stmt));
     }
-  SVN_ERR(svn_sqlite__reset(stmt));
 
   if (b->moved_to_relpath)
     {

commit c40995b244fc87849a4d11453a823c1fc4f3756e

Fix the authz test failure caused by r1340605 by handling a few states directly.
* subversion/libsvn_wc/wc_db.c
  (delete_node): Handle server excluded status directly instead of relying on a
    query to catch this. Add handling for the user excluded status.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1340684 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/wc_db.c
@@ -6492,6 +6492,20 @@ delete_node(void *baton,
         }
       SVN_ERR(svn_sqlite__reset(stmt));
     }
+  else if (status == svn_wc__db_status_server_excluded)
+    {
+      return svn_error_createf(SVN_ERR_WC_PATH_UNEXPECTED_STATUS, NULL,
+                          _("Cannot delete '%s' as it is excluded by server"),
+                               path_for_error_message(wcroot, local_relpath,
+                                                      scratch_pool));
+    }
+  else if (status == svn_wc__db_status_excluded)
+    {
+      return svn_error_createf(SVN_ERR_WC_PATH_UNEXPECTED_STATUS, NULL,
+                          _("Cannot delete '%s' as it is excluded"),
+                               path_for_error_message(wcroot, local_relpath,
+                                                      scratch_pool));
+    }
 
   if (b->moved_to_relpath)
     {