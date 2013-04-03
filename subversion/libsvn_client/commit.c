svn2github/subversion

commit e88a3757f0e0c3696c82b331a49beb983c3d76dc

Fix some sleep-for-timestamps code: avoid missing some cases where we need to sleep -- especially error cases -- and avoid sleeping in some cases where we don't need to.  Simplify the code by using a consistent pattern: make the decision of whether to sleep at the lowest level possible (ideally in the WC layer, but not yet), and do the sleep at the highest level possible (immediately below the libsvn_client API).
Currently we sleep before releasing the WC write lock in some places, and
after releasing it in other places.  We should pick one and be consistent.

* subversion/libsvn_client/client.h
  (svn_client__update_internal,
   svn_client__checkout_internal,
   svn_client__switch_internal,
   svn_client__handle_externals): Document that the 'sleep' output is set
    even if the function returns an error, and don't let it be NULL.
  (svn_client__export_externals): Remove the unused and unneeded 'sleep'
    output parameter.

* subversion/libsvn_client/checkout.c
  (svn_client__checkout_internal): Rely on the caller to sleep even if we
    throw an error. Don't always sleep on success, only if we actually
    update the WC.
  (svn_client_checkout3): Handle the sleep here.

* subversion/libsvn_client/commit.c
  (svn_client_commit6): Sleep, if needed, even if there is an error during
    the post-commit 'bumping' of the WC.

* subversion/libsvn_client/copy.c
  (do_wc_to_wc_copies_with_write_lock,
   do_wc_to_wc_moves,
   repos_to_wc_copy_single): Return a flag instead of sleeping here. A
     multi-target repos-to-WC copy will now only sleep once, not once per
     target.
  (do_wc_to_wc_copies,
   repos_to_wc_copy_locked,
   repos_to_wc_copy,
   try_copy): Pass the flag through.
  (svn_client_copy6,
   svn_client_move7): Handle the sleep here.

* subversion/libsvn_client/export.c
  (export_directory): Remove the unneeded 'sleep' variable.

* subversion/libsvn_client/externals.c
  (svn_client__export_externals): Remove the unused and unneeded 'sleep'

subversion/libsvn_client/commit.c
@@ -985,10 +985,11 @@ svn_client_commit6(const apr_array_header_t *targets,
                    iterpool);
     }
 
-  /* Sleep to ensure timestamp integrity. */
-  svn_io_sleep_for_timestamps(base_abspath, pool);
-
  cleanup:
+  /* Sleep to ensure timestamp integrity. */
+  if (commit_in_progress)
+    svn_io_sleep_for_timestamps(base_abspath, pool);
+
   /* Abort the commit if it is still in progress. */
   svn_pool_clear(iterpool); /* Close open handles before aborting */
   if (commit_in_progress)

subversion/libsvn_client/checkout.c
@@ -78,9 +78,6 @@ svn_client__checkout_internal(svn_revnum_t *result_rev,
                               svn_client_ctx_t *ctx,
                               apr_pool_t *pool)
 {
-  svn_error_t *err = NULL;
-  svn_boolean_t sleep_here = FALSE;
-  svn_boolean_t *use_sleep = timestamp_sleep ? timestamp_sleep : &sleep_here;
   svn_node_kind_t kind;
   apr_pool_t *session_pool = svn_pool_create(pool);
   svn_ra_session_t *ra_session;
@@ -123,7 +120,7 @@ svn_client__checkout_internal(svn_revnum_t *result_rev,
          entries file should only have an entry for THIS_DIR with a
          URL, revnum, and an 'incomplete' flag.  */
       SVN_ERR(svn_io_make_dir_recursively(local_abspath, pool));
-      err = initialize_area(local_abspath, pathrev, depth, ctx, pool);
+      SVN_ERR(initialize_area(local_abspath, pathrev, depth, ctx, pool));
     }
   else if (kind == svn_node_dir)
     {
@@ -133,7 +130,7 @@ svn_client__checkout_internal(svn_revnum_t *result_rev,
       SVN_ERR(svn_wc_check_wc2(&wc_format, ctx->wc_ctx, local_abspath, pool));
       if (! wc_format)
         {
-          err = initialize_area(local_abspath, pathrev, depth, ctx, pool);
+          SVN_ERR(initialize_area(local_abspath, pathrev, depth, ctx, pool));
         }
       else
         {
@@ -160,28 +157,13 @@ svn_client__checkout_internal(svn_revnum_t *result_rev,
     }
 
   /* Have update fix the incompleteness. */
-  if (! err)
-    {
-      err = svn_client__update_internal(result_rev, local_abspath,
-                                        revision, depth, TRUE,
-                                        ignore_externals,
-                                        allow_unver_obstructions,
-                                        TRUE /* adds_as_modification */,
-                                        FALSE, FALSE,
-                                        use_sleep, ctx, pool);
-    }
-
-  if (err)
-    {
-      /* Don't rely on the error handling to handle the sleep later, do
-         it now */
-      svn_io_sleep_for_timestamps(local_abspath, pool);
-      return svn_error_trace(err);
-    }
-  *use_sleep = TRUE;
-
-  if (sleep_here)
-    svn_io_sleep_for_timestamps(local_abspath, pool);
+  SVN_ERR(svn_client__update_internal(result_rev, local_abspath,
+                                      revision, depth, TRUE,
+                                      ignore_externals,
+                                      allow_unver_obstructions,
+                                      TRUE /* adds_as_modification */,
+                                      FALSE, FALSE,
+                                      timestamp_sleep, ctx, pool));
 
   return SVN_NO_ERROR;
 }
@@ -199,12 +181,18 @@ svn_client_checkout3(svn_revnum_t *result_rev,
                      apr_pool_t *pool)
 {
   const char *local_abspath;
+  svn_error_t *err;
+  svn_boolean_t sleep_here = FALSE;
 
   SVN_ERR(svn_dirent_get_absolute(&local_abspath, path, pool));
 
-  return svn_client__checkout_internal(result_rev, URL, local_abspath,
-                                       peg_revision, revision, depth,
-                                       ignore_externals,
-                                       allow_unver_obstructions, NULL,
-                                       ctx, pool);
+  err = svn_client__checkout_internal(result_rev, URL, local_abspath,
+                                      peg_revision, revision, depth,
+                                      ignore_externals,
+                                      allow_unver_obstructions, &sleep_here,
+                                      ctx, pool);
+  if (sleep_here)
+    svn_io_sleep_for_timestamps(local_abspath, pool);
+
+  return svn_error_trace(err);
 }

subversion/libsvn_client/client.h
@@ -467,11 +467,9 @@ svn_client__make_local_parents(const char *path,
 
    If IGNORE_EXTERNALS is true, do no externals processing.
 
-   If TIMESTAMP_SLEEP is NULL this function will sleep before
-   returning to ensure timestamp integrity.  If TIMESTAMP_SLEEP is not
-   NULL then the function will not sleep but will set *TIMESTAMP_SLEEP
-   to TRUE if a sleep is required, and will not change
-   *TIMESTAMP_SLEEP if no sleep is required.
+   Set *TIMESTAMP_SLEEP to TRUE if a sleep is required; otherwise do not
+   change *TIMESTAMP_SLEEP.  The output will be valid even if the function
+   returns an error.
 
    If ALLOW_UNVER_OBSTRUCTIONS is TRUE, unversioned children of LOCAL_ABSPATH
    that obstruct items added from the repos are tolerated; if FALSE,
@@ -522,11 +520,11 @@ svn_client__update_internal(svn_revnum_t *result_rev,
 
    If IGNORE_EXTERNALS is true, do no externals processing.
 
-   If TIMESTAMP_SLEEP is NULL this function will sleep before
-   returning to ensure timestamp integrity.  If TIMESTAMP_SLEEP is not
-   NULL then the function will not sleep but will set *TIMESTAMP_SLEEP
-   to TRUE if a sleep is required, and will not change *TIMESTAMP_SLEEP
-   if no sleep is required.  If ALLOW_UNVER_OBSTRUCTIONS is TRUE,
+   Set *TIMESTAMP_SLEEP to TRUE if a sleep is required; otherwise do not
+   change *TIMESTAMP_SLEEP.  The output will be valid even if the function
+   returns an error.
+
+   If ALLOW_UNVER_OBSTRUCTIONS is TRUE,
    unversioned children of LOCAL_ABSPATH that obstruct items added from
    the repos are tolerated; if FALSE, these obstructions cause the checkout
    to fail.
@@ -551,11 +549,9 @@ svn_client__checkout_internal(svn_revnum_t *result_rev,
    acquired and released if not held. Only switch as deeply as DEPTH
    indicates.
 
-   If TIMESTAMP_SLEEP is NULL this function will sleep before
-   returning to ensure timestamp integrity.  If TIMESTAMP_SLEEP is not
-   NULL then the function will not sleep but will set *TIMESTAMP_SLEEP
-   to TRUE if a sleep is required, and will not change
-   *TIMESTAMP_SLEEP if no sleep is required.
+   Set *TIMESTAMP_SLEEP to TRUE if a sleep is required; otherwise do not
+   change *TIMESTAMP_SLEEP.  The output will be valid even if the function
+   returns an error.
 
    If IGNORE_EXTERNALS is true, don't process externals.
 
@@ -975,9 +971,9 @@ svn_client__do_commit(const char *base_url,
 
    Pass NOTIFY_FUNC with NOTIFY_BATON along to svn_client_checkout().
 
-   *TIMESTAMP_SLEEP will be set TRUE if a sleep is required to ensure
-   timestamp integrity, *TIMESTAMP_SLEEP will be unchanged if no sleep
-   is required.
+   Set *TIMESTAMP_SLEEP to TRUE if a sleep is required; otherwise do not
+   change *TIMESTAMP_SLEEP.  The output will be valid even if the function
+   returns an error.
 
    Use POOL for temporary allocation. */
 svn_error_t *
@@ -1006,10 +1002,6 @@ svn_client__handle_externals(apr_hash_t *externals_new,
 
    NATIVE_EOL is the value passed as NATIVE_EOL when exporting.
 
-   *TIMESTAMP_SLEEP will be set TRUE if a sleep is required to ensure
-   timestamp integrity, *TIMESTAMP_SLEEP will be unchanged if no sleep
-   is required.
-
    Use POOL for temporary allocation. */
 svn_error_t *
 svn_client__export_externals(apr_hash_t *externals,
@@ -1019,7 +1011,6 @@ svn_client__export_externals(apr_hash_t *externals,
                              svn_depth_t requested_depth,
                              const char *native_eol,
                              svn_boolean_t ignore_keywords,
-                             svn_boolean_t *timestamp_sleep,
                              svn_client_ctx_t *ctx,
                              apr_pool_t *pool);
 

subversion/libsvn_client/copy.c
@@ -180,7 +180,8 @@ get_copy_pair_ancestors(const apr_array_header_t *copy_pairs,
 
 /* The guts of do_wc_to_wc_copies */
 static svn_error_t *
-do_wc_to_wc_copies_with_write_lock(const apr_array_header_t *copy_pairs,
+do_wc_to_wc_copies_with_write_lock(svn_boolean_t *timestamp_sleep,
+                                   const apr_array_header_t *copy_pairs,
                                    const char *dst_parent,
                                    svn_client_ctx_t *ctx,
                                    apr_pool_t *scratch_pool)
@@ -203,6 +204,7 @@ do_wc_to_wc_copies_with_write_lock(const apr_array_header_t *copy_pairs,
       /* Perform the copy */
       dst_abspath = svn_dirent_join(pair->dst_parent_abspath, pair->base_name,
                                     iterpool);
+      *timestamp_sleep = TRUE;
       err = svn_wc_copy3(ctx->wc_ctx, pair->src_abspath_or_url, dst_abspath,
                          FALSE /* metadata_only */,
                          ctx->cancel_func, ctx->cancel_baton,
@@ -212,7 +214,6 @@ do_wc_to_wc_copies_with_write_lock(const apr_array_header_t *copy_pairs,
     }
   svn_pool_destroy(iterpool);
 
-  svn_io_sleep_for_timestamps(dst_parent, scratch_pool);
   SVN_ERR(err);
   return SVN_NO_ERROR;
 }
@@ -220,7 +221,8 @@ do_wc_to_wc_copies_with_write_lock(const apr_array_header_t *copy_pairs,
 /* Copy each COPY_PAIR->SRC into COPY_PAIR->DST.  Use POOL for temporary
    allocations. */
 static svn_error_t *
-do_wc_to_wc_copies(const apr_array_header_t *copy_pairs,
+do_wc_to_wc_copies(svn_boolean_t *timestamp_sleep,
+                   const apr_array_header_t *copy_pairs,
                    svn_client_ctx_t *ctx,
                    apr_pool_t *pool)
 {
@@ -233,7 +235,8 @@ do_wc_to_wc_copies(const apr_array_header_t *copy_pairs,
   SVN_ERR(svn_dirent_get_absolute(&dst_parent_abspath, dst_parent, pool));
 
   SVN_WC__CALL_WITH_WRITE_LOCK(
-    do_wc_to_wc_copies_with_write_lock(copy_pairs, dst_parent, ctx, pool),
+    do_wc_to_wc_copies_with_write_lock(timestamp_sleep, copy_pairs, dst_parent,
+                                       ctx, pool),
     ctx->wc_ctx, dst_parent_abspath, FALSE, pool);
 
   return SVN_NO_ERROR;
@@ -295,7 +298,8 @@ do_wc_to_wc_moves_with_locks1(svn_client__copy_pair_t *pair,
 /* Move each COPY_PAIR->SRC into COPY_PAIR->DST, deleting COPY_PAIR->SRC
    afterwards.  Use POOL for temporary allocations. */
 static svn_error_t *
-do_wc_to_wc_moves(const apr_array_header_t *copy_pairs,
+do_wc_to_wc_moves(svn_boolean_t *timestamp_sleep,
+                  const apr_array_header_t *copy_pairs,
                   const char *dst_path,
                   svn_boolean_t allow_mixed_revisions,
                   svn_boolean_t metadata_only,
@@ -349,6 +353,8 @@ do_wc_to_wc_moves(const apr_array_header_t *copy_pairs,
           lock_dst = TRUE;
         }
 
+      *timestamp_sleep = TRUE;
+
       /* Perform the copy and then the delete. */
       if (lock_src)
         SVN_WC__CALL_WITH_WRITE_LOCK(
@@ -368,8 +374,6 @@ do_wc_to_wc_moves(const apr_array_header_t *copy_pairs,
 
     }
   svn_pool_destroy(iterpool);
-
-  svn_io_sleep_for_timestamps(dst_path, pool);
 
   return svn_error_trace(err);
 }
@@ -1540,7 +1544,8 @@ notification_adjust_func(void *baton,
 
    Resolve PAIR->src_revnum to a real revision number if it isn't already. */
 static svn_error_t *
-repos_to_wc_copy_single(svn_client__copy_pair_t *pair,
+repos_to_wc_copy_single(svn_boolean_t *timestamp_sleep,
+                        svn_client__copy_pair_t *pair,
                         svn_boolean_t same_repositories,
                         svn_boolean_t ignore_externals,
                         svn_ra_session_t *ra_session,
@@ -1615,6 +1620,8 @@ repos_to_wc_copy_single(svn_client__copy_pair_t *pair,
             SVN_ERR(err);
           }
 
+          *timestamp_sleep = TRUE;
+
       /* Schedule dst_path for addition in parent, with copy history.
          Don't send any notification here.
          Then remove the temporary checkout's .svn dir in preparation for
@@ -1637,6 +1644,8 @@ repos_to_wc_copy_single(svn_client__copy_pair_t *pair,
         }
       else
         {
+          *timestamp_sleep = TRUE;
+
           SVN_ERR(svn_client__copy_foreign(pair->src_abspath_or_url,
                                            dst_abspath,
                                            &pair->src_peg_revision,
@@ -1668,6 +1677,8 @@ repos_to_wc_copy_single(svn_client__copy_pair_t *pair,
       if (new_props && ! same_repositories)
         svn_hash_sets(new_props, SVN_PROP_MERGEINFO, NULL);
 
+      *timestamp_sleep = TRUE;
+
       SVN_ERR(svn_wc_add_repos_file4(
          ctx->wc_ctx, dst_abspath,
          new_base_contents, NULL, new_props, NULL,
@@ -1697,13 +1708,12 @@ repos_to_wc_copy_single(svn_client__copy_pair_t *pair,
       (*ctx->notify_func2)(ctx->notify_baton2, notify, pool);
     }
 
-  svn_io_sleep_for_timestamps(dst_abspath, pool);
-
   return SVN_NO_ERROR;
 }
 
-static svn_error_t*
-repos_to_wc_copy_locked(const apr_array_header_t *copy_pairs,
+static svn_error_t *
+repos_to_wc_copy_locked(svn_boolean_t *timestamp_sleep,
+                        const apr_array_header_t *copy_pairs,
                         const char *top_dst_path,
                         svn_boolean_t ignore_externals,
                         svn_ra_session_t *ra_session,
@@ -1766,7 +1776,8 @@ repos_to_wc_copy_locked(const apr_array_header_t *copy_pairs,
 
       svn_pool_clear(iterpool);
 
-      SVN_ERR(repos_to_wc_copy_single(APR_ARRAY_IDX(copy_pairs, i,
+      SVN_ERR(repos_to_wc_copy_single(timestamp_sleep,
+                                      APR_ARRAY_IDX(copy_pairs, i,
                                                     svn_client__copy_pair_t *),
                                       same_repositories,
                                       ignore_externals,
@@ -1778,7 +1789,8 @@ repos_to_wc_copy_locked(const apr_array_header_t *copy_pairs,
 }
 
 static svn_error_t *
-repos_to_wc_copy(const apr_array_header_t *copy_pairs,
+repos_to_wc_copy(svn_boolean_t *timestamp_sleep,
+                 const apr_array_header_t *copy_pairs,
                  svn_boolean_t make_parents,
                  svn_boolean_t ignore_externals,
                  svn_client_ctx_t *ctx,
@@ -1887,7 +1899,8 @@ repos_to_wc_copy(const apr_array_header_t *copy_pairs,
   svn_pool_destroy(iterpool);
 
   SVN_WC__CALL_WITH_WRITE_LOCK(
-    repos_to_wc_copy_locked(copy_pairs, top_dst_path, ignore_externals,
+    repos_to_wc_copy_locked(timestamp_sleep,
+                            copy_pairs, top_dst_path, ignore_externals,
                             ra_session, ctx, pool),
     ctx->wc_ctx, lock_abspath, FALSE, pool);
   return SVN_NO_ERROR;
@@ -1899,7 +1912,8 @@ repos_to_wc_copy(const apr_array_header_t *copy_pairs,
 
 /* Perform all allocations in POOL. */
 static svn_error_t *
-try_copy(const apr_array_header_t *sources,
+try_copy(svn_boolean_t *timestamp_sleep,
+         const apr_array_header_t *sources,
          const char *dst_path_in,
          svn_boolean_t is_move,
          svn_boolean_t allow_mixed_revisions,
@@ -2193,7 +2207,8 @@ try_copy(const apr_array_header_t *sources,
 
       /* Copy or move all targets. */
       if (is_move)
-        return svn_error_trace(do_wc_to_wc_moves(copy_pairs, dst_path_in,
+        return svn_error_trace(do_wc_to_wc_moves(timestamp_sleep,
+                                                 copy_pairs, dst_path_in,
                                                  allow_mixed_revisions,
                                                  metadata_only,
                                                  ctx, pool));
@@ -2201,7 +2216,8 @@ try_copy(const apr_array_header_t *sources,
         {
           /* We ignore these values, so assert the default value */
           SVN_ERR_ASSERT(allow_mixed_revisions && !metadata_only);
-          return svn_error_trace(do_wc_to_wc_copies(copy_pairs, ctx, pool));
+          return svn_error_trace(do_wc_to_wc_copies(timestamp_sleep,
+                                                    copy_pairs, ctx, pool));
         }
     }
   else if ((! srcs_are_urls) && (dst_is_url))
@@ -2213,7 +2229,8 @@ try_copy(const apr_array_header_t *sources,
   else if ((srcs_are_urls) && (! dst_is_url))
     {
       return svn_error_trace(
-        repos_to_wc_copy(copy_pairs, make_parents, ignore_externals,
+        repos_to_wc_copy(timestamp_sleep,
+                         copy_pairs, make_parents, ignore_externals,
                          ctx, pool));
     }
   else
@@ -2241,13 +2258,15 @@ svn_client_copy6(const apr_array_header_t *sources,
                  apr_pool_t *pool)
 {
   svn_error_t *err;
+  svn_boolean_t timestamp_sleep;
   apr_pool_t *subpool = svn_pool_create(pool);
 
   if (sources->nelts > 1 && !copy_as_child)
     return svn_error_create(SVN_ERR_CLIENT_MULTIPLE_SOURCES_DISALLOWED,
                             NULL, NULL);
 
-  err = try_copy(sources, dst_path,
+  err = try_copy(&timestamp_sleep,
+                 sources, dst_path,
                  FALSE /* is_move */,
                  TRUE /* allow_mixed_revisions */,
                  FALSE /* metadata_only */,
@@ -2276,7 +2295,8 @@ svn_client_copy6(const apr_array_header_t *sources,
       src_basename = src_is_url ? svn_uri_basename(src_path, subpool)
                                 : svn_dirent_basename(src_path, subpool);
 
-      err = try_copy(sources,
+      err = try_copy(&timestamp_sleep,
+                     sources,
                      dst_is_url
                          ? svn_path_url_add_component2(dst_path, src_basename,
                                                        subpool)
@@ -2291,6 +2311,9 @@ svn_client_copy6(const apr_array_header_t *sources,
                      ctx,
                      subpool);
     }
+
+  if (timestamp_sleep)
+    svn_io_sleep_for_timestamps(NULL, subpool);
 
   svn_pool_destroy(subpool);
   return svn_error_trace(err);
@@ -2313,6 +2336,7 @@ svn_client_move7(const apr_array_header_t *src_paths,
   const svn_opt_revision_t head_revision
     = { svn_opt_revision_head, { 0 } };
   svn_error_t *err;
+  svn_boolean_t timestamp_sleep;
   int i;
   apr_pool_t *subpool = svn_pool_create(pool);
   apr_array_header_t *sources = apr_array_make(pool, src_paths->nelts,
@@ -2335,7 +2359,8 @@ svn_client_move7(const apr_array_header_t *src_paths,
       APR_ARRAY_PUSH(sources, svn_client_copy_source_t *) = copy_source;
     }
 
-  err = try_copy(sources, dst_path,
+  err = try_copy(&timestamp_sleep,
+                 sources, dst_path,
                  TRUE /* is_move */,
                  allow_mixed_revisions,
                  metadata_only,
@@ -2363,7 +2388,8 @@ svn_client_move7(const apr_array_header_t *src_paths,
       src_basename = src_is_url ? svn_uri_basename(src_path, pool)
                                 : svn_dirent_basename(src_path, pool);
 
-      err = try_copy(sources,
+      err = try_copy(&timestamp_sleep,
+                     sources,
                      dst_is_url
                          ? svn_path_url_add_component2(dst_path,
                                                        src_basename, pool)
@@ -2378,6 +2404,9 @@ svn_client_move7(const apr_array_header_t *src_paths,
                      ctx,
                      subpool);
     }
+
+  if (timestamp_sleep)
+    svn_io_sleep_for_timestamps(NULL, subpool);
 
   svn_pool_destroy(subpool);
   return svn_error_trace(err);

subversion/libsvn_client/export.c
@@ -1296,7 +1296,6 @@ export_directory(const char *from_path_or_url,
   const svn_delta_editor_t *export_editor;
   const svn_ra_reporter3_t *reporter;
   void *report_baton;
-  svn_boolean_t use_sleep = FALSE;
   svn_node_kind_t kind;
 
   if (!ENABLE_EV2_IMPL)
@@ -1351,7 +1350,7 @@ export_directory(const char *from_path_or_url,
                                            from_path_or_url,
                                            to_abspath, repos_root_url,
                                            depth, native_eol,
-                                           ignore_keywords, &use_sleep,
+                                           ignore_keywords,
                                            ctx, scratch_pool));
     }
 

subversion/libsvn_client/externals.c
@@ -1048,7 +1048,6 @@ svn_client__export_externals(apr_hash_t *externals,
                              svn_depth_t requested_depth,
                              const char *native_eol,
                              svn_boolean_t ignore_keywords,
-                             svn_boolean_t *timestamp_sleep,
                              svn_client_ctx_t *ctx,
                              apr_pool_t *scratch_pool)
 {

subversion/libsvn_client/revert.c
@@ -160,7 +160,9 @@ svn_client_revert2(const apr_array_header_t *paths,
           && ((err = ctx->cancel_func(ctx->cancel_baton))))
         goto errorful;
 
-      SVN_ERR(svn_dirent_get_absolute(&local_abspath, path, pool));
+      err = svn_dirent_get_absolute(&local_abspath, path, pool);
+      if (err)
+        goto errorful;
 
       baton.local_abspath = local_abspath;
       baton.depth = depth;
@@ -168,8 +170,9 @@ svn_client_revert2(const apr_array_header_t *paths,
       baton.changelists = changelists;
       baton.ctx = ctx;
 
-      SVN_ERR(svn_wc__is_wcroot(&wc_root, ctx->wc_ctx, local_abspath,
-                                pool));
+      err = svn_wc__is_wcroot(&wc_root, ctx->wc_ctx, local_abspath, pool);
+      if (err)
+        goto errorful;
       lock_target = wc_root ? local_abspath
                             : svn_dirent_dirname(local_abspath, pool);
       err = svn_wc__call_with_write_lock(revert, &baton, ctx->wc_ctx,

subversion/libsvn_client/switch.c
@@ -105,13 +105,10 @@ switch_internal(svn_revnum_t *result_rev,
   svn_client__pathrev_t *switch_loc;
   svn_ra_session_t *ra_session;
   svn_revnum_t revnum;
-  svn_error_t *err = SVN_NO_ERROR;
   const char *diff3_cmd;
   apr_hash_t *wcroot_iprops;
   apr_array_header_t *inherited_props;
   svn_boolean_t use_commit_times;
-  svn_boolean_t sleep_here = FALSE;
-  svn_boolean_t *use_sleep = timestamp_sleep ? timestamp_sleep : &sleep_here;
   const svn_delta_editor_t *switch_editor;
   void *switch_edit_baton;
   const char *preserved_exts_str;
@@ -338,24 +335,22 @@ switch_internal(svn_revnum_t *result_rev,
                             switch_editor, switch_edit_baton,
                             pool, pool));
 
+  /* Past this point, we assume the WC is going to be modified so we will
+   * need to sleep for timestamps. */
+  if (! use_commit_times)
+    *timestamp_sleep = TRUE;
+
   /* Drive the reporter structure, describing the revisions within
      PATH.  When we call reporter->finish_report, the update_editor
      will be driven by svn_repos_dir_delta2. */
-  err = svn_wc_crawl_revisions5(ctx->wc_ctx, local_abspath, reporter,
-                                report_baton, TRUE, depth, (! depth_is_sticky),
-                                (! server_supports_depth),
-                                use_commit_times,
-                                ctx->cancel_func, ctx->cancel_baton,
-                                ctx->notify_func2, ctx->notify_baton2, pool);
-
-  if (err)
-    {
-      /* Don't rely on the error handling to handle the sleep later, do
-         it now */
-      svn_io_sleep_for_timestamps(local_abspath, pool);
-      return svn_error_trace(err);
-    }
-  *use_sleep = TRUE;
+  SVN_ERR(svn_wc_crawl_revisions5(ctx->wc_ctx, local_abspath, reporter,
+                                  report_baton, TRUE,
+                                  depth, (! depth_is_sticky),
+                                  (! server_supports_depth),
+                                  use_commit_times,
+                                  ctx->cancel_func, ctx->cancel_baton,
+                                  ctx->notify_func2, ctx->notify_baton2,
+                                  pool));
 
   /* We handle externals after the switch is complete, so that
      handling external items (and any errors therefrom) doesn't delay
@@ -373,18 +368,9 @@ switch_internal(svn_revnum_t *result_rev,
                                            new_depths,
                                            switch_loc->repos_root_url,
                                            local_abspath,
-                                           depth, use_sleep,
+                                           depth, timestamp_sleep,
                                            ctx, pool));
     }
-
-  /* Sleep to ensure timestamp integrity (we do this regardless of
-     errors in the actual switch operation(s)). */
-  if (sleep_here)
-    svn_io_sleep_for_timestamps(local_abspath, pool);
-
-  /* Return errors we might have sustained. */
-  if (err)
-    return svn_error_trace(err);
 
   /* Let everyone know we're finished here. */
   if (ctx->notify_func2)
@@ -480,13 +466,23 @@ svn_client_switch3(svn_revnum_t *result_rev,
                    svn_client_ctx_t *ctx,
                    apr_pool_t *pool)
 {
+  svn_error_t *err;
+  svn_boolean_t sleep_here = FALSE;
+
   if (svn_path_is_url(path))
     return svn_error_createf(SVN_ERR_ILLEGAL_TARGET, NULL,
                              _("'%s' is not a local path"), path);
 
-  return svn_client__switch_internal(result_rev, path, switch_url,
-                                     peg_revision, revision, depth,
-                                     depth_is_sticky, ignore_externals,
-                                     allow_unver_obstructions,
-                                     ignore_ancestry, NULL, ctx, pool);
+  err = svn_client__switch_internal(result_rev, path, switch_url,
+                                    peg_revision, revision, depth,
+                                    depth_is_sticky, ignore_externals,
+                                    allow_unver_obstructions,
+                                    ignore_ancestry, &sleep_here, ctx, pool);
+
+  /* Sleep to ensure timestamp integrity (we do this regardless of
+     errors in the actual switch operation(s)). */
+  if (sleep_here)
+    svn_io_sleep_for_timestamps(path, pool);
+
+  return svn_error_trace(err);
 }

commit 2f37717905fdaa7c6ccc54d94225d80f72a15e81

* subversion/libsvn_client/commit.c   (svn_client_commit6): Fix a sleep-for-timestamps bug I introduced in     r1463721.
Found by: philip

git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1463803 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/commit.c
@@ -586,6 +586,7 @@ svn_client_commit6(const apr_array_header_t *targets,
   svn_error_t *bump_err = SVN_NO_ERROR;
   svn_error_t *unlock_err = SVN_NO_ERROR;
   svn_boolean_t commit_in_progress = FALSE;
+  svn_boolean_t timestamp_sleep = FALSE;
   svn_commit_info_t *commit_info = NULL;
   apr_pool_t *iterpool = svn_pool_create(pool);
   const char *current_abspath;
@@ -945,6 +946,11 @@ svn_client_commit6(const apr_array_header_t *targets,
   /* Make a note that we have a commit-in-progress. */
   commit_in_progress = TRUE;
 
+  /* We'll assume that, once we pass this point, we are going to need to
+   * sleep for timestamps.  Really, we may not need to do unless and until
+   * we reach the point where we post-commit 'bump' the WC metadata. */
+  timestamp_sleep = TRUE;
+
   /* Perform the commit. */
   cmt_err = svn_error_trace(
               svn_client__do_commit(base_url, commit_items, editor, edit_baton,
@@ -987,7 +993,7 @@ svn_client_commit6(const apr_array_header_t *targets,
 
  cleanup:
   /* Sleep to ensure timestamp integrity. */
-  if (commit_in_progress)
+  if (timestamp_sleep)
     svn_io_sleep_for_timestamps(base_abspath, pool);
 
   /* Abort the commit if it is still in progress. */