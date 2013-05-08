svn2github/subversion

commit 8866077bb5f9411e5a562ba2f245ba5692f0e0ca

Start using a scratch pool parameter that was introduced in r1097039.
* subversion/libsvn_client/repos_diff.c
  (get_file_from_ra): Use the 'scratch_pool' parameter where possible.
  (apply_textdelta): Pass the local 'scratch_pool' to it. This change
    currently has no effect because the 'scratch_pool' variable used here is
    simply a copy of the 'b->pool' that was previously used here. But the
    other two callers of get_file_from_ra() are passing real scratch pools.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1149264 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/repos_diff.c
@@ -334,11 +334,11 @@ get_file_from_ra(struct file_baton *b,
       svn_stream_t *fstream;
 
       SVN_ERR(svn_stream_open_unique(&fstream, &(b->path_start_revision), NULL,
-                                     svn_io_file_del_on_pool_cleanup, b->pool,
-                                     b->pool));
+                                     svn_io_file_del_on_pool_cleanup,
+                                     scratch_pool, scratch_pool));
 
       fstream = svn_stream_checksummed2(fstream, NULL, &b->start_md5_checksum,
-                                        svn_checksum_md5, TRUE, b->pool);
+                                        svn_checksum_md5, TRUE, scratch_pool);
 
       /* Retrieve the file and its properties */
       SVN_ERR(svn_ra_get_file(b->edit_baton->ra_session,
@@ -898,7 +898,7 @@ apply_textdelta(void *file_baton,
 
   /* We need the expected pristine file, so go get it */
   if (!b->added)
-    SVN_ERR(get_file_from_ra(b, FALSE, b->pool));
+    SVN_ERR(get_file_from_ra(b, FALSE, scratch_pool));
   else
     SVN_ERR(get_empty_file(b->edit_baton, &(b->path_start_revision)));
 

commit 4095496a1b11b7cf316a092647276a072ea019c6

Fix a dormant pool usage bug that I introduced in r1149264 and partially fixed in r1159672. The current callers were all safe.
* subversion/libsvn_client/repos_diff.c
  (get_file_from_ra): Allocate the checksum result in 'fb->pool', not in
    'scratch_pool'.

git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1480416 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/repos_diff.c
@@ -328,7 +328,7 @@ get_file_from_ra(struct file_baton *fb,
                                      fb->pool, scratch_pool));
 
       fstream = svn_stream_checksummed2(fstream, NULL, &fb->start_md5_checksum,
-                                        svn_checksum_md5, TRUE, scratch_pool);
+                                        svn_checksum_md5, TRUE, fb->pool);
 
       /* Retrieve the file and its properties */
       SVN_ERR(svn_ra_get_file(fb->edit_baton->ra_session,