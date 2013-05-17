svn2github/subversion

commit ff84551e82d73097c07c97276e632b0327a93c56

* subversion/include/svn_auth.h   (SVN_AUTH_PARAM_CONFIG_CATEGORY_CONFIG): Improve documentation.   (SVN_AUTH_PARAM_CONFIG_CATEGROY_SERVERS): Add documentation.
git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1484017 13f79535-47bb-0310-9956-ffa450edef68

subversion/include/svn_auth.h
@@ -628,8 +628,13 @@ svn_auth_get_parameter(svn_auth_baton_t *auth_baton,
 #define SVN_AUTH_PARAM_SSL_SERVER_CERT_INFO SVN_AUTH_PARAM_PREFIX \
   "ssl:cert-info"
 
-/** Some providers need access to the @c svn_config_t configuration. */
-#define SVN_AUTH_PARAM_CONFIG_CATEGORY_CONFIG SVN_AUTH_PARAM_PREFIX "config-category-config"
+/** This provides a pointer to a @c svn_config_t containting the config
+ * category. */
+#define SVN_AUTH_PARAM_CONFIG_CATEGORY_CONFIG SVN_AUTH_PARAM_PREFIX
+"config-category-config"
+
+/** This provides a pointer to a @c svn_config_t containting the servers
+ * category. */
 #define SVN_AUTH_PARAM_CONFIG_CATEGORY_SERVERS SVN_AUTH_PARAM_PREFIX "config-category-servers"
 
 /** @deprecated Provided for backward compatibility with the 1.5 API. */

commit 7c7bf021a8f3da6790754ddfed67f9611dbb19e5

Fix accidental breakage from r1484017
* subversion/include/svn_auth.h
  (SVN_AUTH_PARAM_CONFIG_CATEGORY_CONFIG, SVN_AUTH_PARAM_CONFIG_CATEGORY_SERVERS):
    Wrap long lines properly.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1484023 13f79535-47bb-0310-9956-ffa450edef68

subversion/include/svn_auth.h
@@ -630,12 +630,13 @@ svn_auth_get_parameter(svn_auth_baton_t *auth_baton,
 
 /** This provides a pointer to a @c svn_config_t containting the config
  * category. */
-#define SVN_AUTH_PARAM_CONFIG_CATEGORY_CONFIG SVN_AUTH_PARAM_PREFIX
-"config-category-config"
+#define SVN_AUTH_PARAM_CONFIG_CATEGORY_CONFIG SVN_AUTH_PARAM_PREFIX \
+  "config-category-config"
 
 /** This provides a pointer to a @c svn_config_t containting the servers
  * category. */
-#define SVN_AUTH_PARAM_CONFIG_CATEGORY_SERVERS SVN_AUTH_PARAM_PREFIX "config-category-servers"
+#define SVN_AUTH_PARAM_CONFIG_CATEGORY_SERVERS SVN_AUTH_PARAM_PREFIX \
+  "config-category-servers"
 
 /** @deprecated Provided for backward compatibility with the 1.5 API. */
 #define SVN_AUTH_PARAM_CONFIG SVN_AUTH_PARAM_CONFIG_CATEGORY_SERVERS