svn2github/subversion

commit 6f7ff274f5ed0f6d764e4c69872c329d01600c9c

Merge revisions r1433848,1438408,1445080 from branches/fsfs-format7. These introduce svn_io_file_create_empty and svn_io_file_create_binary as alternatives to svn_io_file_create.
A few conflicts had to be resolved.

-- Copied from the merges
* subversion/include/svn_io.h
  (svn_io_file_create_empty,
   svn_io_file_create_binary): New functions. Variants of svn_io_file_create.

* subversion/libsvn_subr/io.c
  (svn_io_file_create_binary): implement most generic function.
  (svn_io_file_create,
   svn_io_file_create_empty): call the above.

* subversion/libsvn_client/patch.c
  (install_patched_prop_targets): update caller

* subversion/libsvn_fs_fs/fs.c
  (fs_open_for_recovery): ditto

* subversion/libsvn_fs_fs/fs_fs.c
  (write_config,
   create_file_ignore_eexist,
   upgrade_body,
   write_revision_zero,
   svn_fs_fs__create): same here

* subversion/libsvn_fs_fs/hotcopy.c
  (hotcopy_create_empty_dest): here

* subversion/libsvn_fs_fs/transaction.c
  (get_lock_on_filesystem,
   svn_fs_fs__create_txn): here

* subversion/libsvn_repos/repos.c
  (create_db_logs_lock,
   create_db_lock,
   create_conf): and here

* subversion/libsvn_wc/adm_files.c
  (init_adm): and finally here.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1505006 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_subr/io.c
@@ -1166,9 +1166,10 @@ svn_io_make_dir_recursively(const char *path, apr_pool_t *pool)
   return SVN_NO_ERROR;
 }
 
-svn_error_t *svn_io_file_create(const char *file,
-                                const char *contents,
-                                apr_pool_t *pool)
+svn_error_t *svn_io_file_create_binary(const char *file,
+                                       const char *contents,
+                                       apr_size_t length,
+                                       apr_pool_t *pool)
 {
   apr_file_t *f;
   apr_size_t written;
@@ -1178,14 +1179,26 @@ svn_error_t *svn_io_file_create(const char *file,
                            (APR_WRITE | APR_CREATE | APR_EXCL),
                            APR_OS_DEFAULT,
                            pool));
-  if (contents && *contents)
-    err = svn_io_file_write_full(f, contents, strlen(contents),
-                                 &written, pool);
-
+  if (length)
+    err = svn_io_file_write_full(f, contents, length, &written, pool);
 
   return svn_error_trace(
                         svn_error_compose_create(err,
                                                  svn_io_file_close(f, pool)));
+}
+
+svn_error_t *svn_io_file_create(const char *file,
+                                const char *contents,
+                                apr_pool_t *pool)
+{
+  return svn_error_trace(svn_io_file_create_binary(file, contents,
+                                                   strlen(contents), pool));
+}
+
+svn_error_t *svn_io_file_create_empty(const char *file,
+                                      apr_pool_t *pool)
+{
+  return svn_error_trace(svn_io_file_create_binary(file, "", 0, pool));
 }
 
 svn_error_t *svn_io_dir_file_copy(const char *src_path,

subversion/include/svn_io.h
@@ -691,6 +691,27 @@ svn_io_file_create(const char *file,
                    const char *contents,
                    apr_pool_t *pool);
 
+/** Create file at utf8-encoded @a file with binary contents @a contents
+ * of @a length bytes.  @a file must not already exist.
+ * Use @a pool for memory allocations.
+ *
+ * @since New in 1.9.
+ */
+svn_error_t *
+svn_io_file_create_binary(const char *file,
+                          const char *contents,
+                          apr_size_t length,
+                          apr_pool_t *pool);
+
+/** Create empty file at utf8-encoded @a file, which must not already exist.
+ * Use @a pool for memory allocations.
+ *
+ * @since New in 1.9.
+ */
+svn_error_t *
+svn_io_file_create_empty(const char *file,
+                         apr_pool_t *pool);
+
 /**
  * Lock file at @a lock_file. If @a exclusive is TRUE,
  * obtain exclusive lock, otherwise obtain shared lock.

subversion/libsvn_client/patch.c
@@ -2676,8 +2676,8 @@ install_patched_prop_targets(patch_target_t *target,
         {
           if (! dry_run)
             {
-              SVN_ERR(svn_io_file_create(target->local_abspath, "",
-                                         scratch_pool));
+              SVN_ERR(svn_io_file_create_empty(target->local_abspath,
+                                               scratch_pool));
               SVN_ERR(svn_wc_add_from_disk2(ctx->wc_ctx, target->local_abspath,
                                             NULL /*props*/,
                                             /* suppress notification */

subversion/libsvn_fs_fs/fs_fs.c
@@ -602,7 +602,7 @@ get_lock_on_filesystem(const char *lock_filename,
       svn_error_clear(err);
       err = NULL;
 
-      SVN_ERR(svn_io_file_create(lock_filename, "", pool));
+      SVN_ERR(svn_io_file_create_empty(lock_filename, pool));
       SVN_ERR(svn_io_file_lock2(lock_filename, TRUE, FALSE, pool));
     }
 
@@ -6633,16 +6633,16 @@ svn_fs_fs__create_txn(svn_fs_txn_t **txn_p,
   SVN_ERR(create_new_txn_noderev_from_rev(fs, txn->id, root_id, pool));
 
   /* Create an empty rev file. */
-  SVN_ERR(svn_io_file_create(path_txn_proto_rev(fs, txn->id, pool), "",
-                             pool));
+  SVN_ERR(svn_io_file_create_empty(path_txn_proto_rev(fs, txn->id, pool),
+                                   pool));
 
   /* Create an empty rev-lock file. */
-  SVN_ERR(svn_io_file_create(path_txn_proto_rev_lock(fs, txn->id, pool), "",
-                             pool));
+  SVN_ERR(svn_io_file_create_empty(path_txn_proto_rev_lock(fs, txn->id, pool),
+                                   pool));
 
   /* Create an empty changes file. */
-  SVN_ERR(svn_io_file_create(path_txn_changes(fs, txn->id, pool), "",
-                             pool));
+  SVN_ERR(svn_io_file_create_empty(path_txn_changes(fs, txn->id, pool),
+                                   pool));
 
   /* Create the next-ids file. */
   return svn_io_file_create(path_txn_next_ids(fs, txn->id, pool), "0 0\n",
@@ -8867,7 +8867,7 @@ svn_fs_fs__create(svn_fs_t *fs,
                              (format >= SVN_FS_FS__MIN_NO_GLOBAL_IDS_FORMAT
                               ? "0\n" : "0 1 1\n"),
                              pool));
-  SVN_ERR(svn_io_file_create(path_lock(fs, pool), "", pool));
+  SVN_ERR(svn_io_file_create_empty(path_lock(fs, pool), pool));
   SVN_ERR(svn_fs_fs__set_uuid(fs, NULL, pool));
 
   SVN_ERR(write_revision_zero(fs));
@@ -8884,10 +8884,8 @@ svn_fs_fs__create(svn_fs_t *fs,
      the transaction sequence file. */
   if (format >= SVN_FS_FS__MIN_TXN_CURRENT_FORMAT)
     {
-      SVN_ERR(svn_io_file_create(path_txn_current(fs, pool),
-                                 "0\n", pool));
-      SVN_ERR(svn_io_file_create(path_txn_current_lock(fs, pool),
-                                 "", pool));
+      SVN_ERR(svn_io_file_create(path_txn_current(fs, pool), "0\n", pool));
+      SVN_ERR(svn_io_file_create_empty(path_txn_current_lock(fs, pool), pool));
     }
 
   /* This filesystem is ready.  Stamp it with a format number. */
@@ -11482,7 +11480,7 @@ hotcopy_create_empty_dest(svn_fs_t *src_fs,
                              pool));
 
   /* Create lock file and UUID. */
-  SVN_ERR(svn_io_file_create(path_lock(dst_fs, pool), "", pool));
+  SVN_ERR(svn_io_file_create_empty(path_lock(dst_fs, pool), pool));
   SVN_ERR(svn_fs_fs__set_uuid(dst_fs, src_fs->uuid, pool));
 
   /* Create the min unpacked rev file. */
@@ -11495,8 +11493,8 @@ hotcopy_create_empty_dest(svn_fs_t *src_fs,
     {
       SVN_ERR(svn_io_file_create(path_txn_current(dst_fs, pool),
                                  "0\n", pool));
-      SVN_ERR(svn_io_file_create(path_txn_current_lock(dst_fs, pool),
-                                 "", pool));
+      SVN_ERR(svn_io_file_create_empty(path_txn_current_lock(dst_fs, pool),
+                                       pool));
     }
 
   dst_ffd->youngest_rev_cache = 0;

commit 5519392a13f7f6036cdb243abd9e659449cb0adc

Apply some tweaks to svn_io_file_create*() after r1505006. Resolve a regression on writing a NULL buffer.
* subversion/libsvn_subr/io.c
  (svn_io_file_create_binary): Try to delete the file we created on error
    writing it. Use standard notation for function prototype.

  (svn_io_file_create): Resolve regression introduced in r1505006, where a
    NULL contents of 0 length would previously not segfault, but now will.
    Use standard notation for function prototype.

  (svn_io_file_create,
   svn_io_dir_file_copy): Use standard notation for function prototype.
     Add svn_error_trace.


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1505219 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_subr/io.c
@@ -1166,10 +1166,11 @@ svn_io_make_dir_recursively(const char *path, apr_pool_t *pool)
   return SVN_NO_ERROR;
 }
 
-svn_error_t *svn_io_file_create_binary(const char *file,
-                                       const char *contents,
-                                       apr_size_t length,
-                                       apr_pool_t *pool)
+svn_error_t *
+svn_io_file_create_binary(const char *file,
+                          const char *contents,
+                          apr_size_t length,
+                          apr_pool_t *pool)
 {
   apr_file_t *f;
   apr_size_t written;
@@ -1182,34 +1183,54 @@ svn_error_t *svn_io_file_create_binary(const char *file,
   if (length)
     err = svn_io_file_write_full(f, contents, length, &written, pool);
 
-  return svn_error_trace(
-                        svn_error_compose_create(err,
-                                                 svn_io_file_close(f, pool)));
+  err = svn_error_compose_create(
+                    err,
+                    svn_io_file_close(f, pool));
+
+  if (err)
+    {
+      /* Our caller doesn't know if we left a file or not if we return
+         an error. Better to cleanup after ourselves if we created the
+         file. */
+      return svn_error_trace(
+                svn_error_compose_create(
+                    err,
+                    svn_io_remove_file2(file, TRUE, pool)));
+    }
+
+  return SVN_NO_ERROR;
 }
 
-svn_error_t *svn_io_file_create(const char *file,
-                                const char *contents,
-                                apr_pool_t *pool)
+svn_error_t *
+svn_io_file_create(const char *file,
+                   const char *contents,
+                   apr_pool_t *pool)
 {
   return svn_error_trace(svn_io_file_create_binary(file, contents,
-                                                   strlen(contents), pool));
+                                                   contents
+                                                        ? strlen(contents)
+                                                        : 0,
+                                                   pool));
 }
 
-svn_error_t *svn_io_file_create_empty(const char *file,
-                                      apr_pool_t *pool)
+svn_error_t *
+svn_io_file_create_empty(const char *file,
+                         apr_pool_t *pool)
 {
   return svn_error_trace(svn_io_file_create_binary(file, "", 0, pool));
 }
 
-svn_error_t *svn_io_dir_file_copy(const char *src_path,
-                                  const char *dest_path,
-                                  const char *file,
-                                  apr_pool_t *pool)
+svn_error_t *
+svn_io_dir_file_copy(const char *src_path,
+                     const char *dest_path,
+                     const char *file,
+                     apr_pool_t *pool)
 {
   const char *file_dest_path = svn_dirent_join(dest_path, file, pool);
   const char *file_src_path = svn_dirent_join(src_path, file, pool);
 
-  return svn_io_copy_file(file_src_path, file_dest_path, TRUE, pool);
+  return svn_error_trace(
+            svn_io_copy_file(file_src_path, file_dest_path, TRUE, pool));
 }
 
 