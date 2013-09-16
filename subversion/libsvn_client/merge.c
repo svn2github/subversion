svn2github/subversion

commit f1b6b329585f735c75c25992e981cf9409ce5ed9

* subversion/libsvn_client/merge.c:   (log_find_operative_revs): Slightly simplify some path arithmetic.
git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1311669 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/merge.c
@@ -9728,7 +9728,7 @@ log_find_operative_revs(void *baton,
       if (!in_catalog)
         {
           svn_mergeinfo_t unmerged_for_key;
-          const char *missing_path;
+          const char *suffix, *missing_path;
 
           /* If there is no mergeinfo on the source tree we'll say
              the "subtree" missing this revision is the root of the
@@ -9736,12 +9736,10 @@ log_find_operative_revs(void *baton,
           if (!subtree_missing_this_rev)
             subtree_missing_this_rev = log_baton->source_repos_rel_path;
 
-          if (subtree_missing_this_rev
-              && strcmp(subtree_missing_this_rev, source_rel_path))
+          suffix = svn_relpath_skip_ancestor(subtree_missing_this_rev,
+                                             source_rel_path);
+          if (suffix)
             {
-              const char *suffix =
-                svn_relpath_skip_ancestor(subtree_missing_this_rev,
-                                          source_rel_path);
               missing_path = apr_pstrmemdup(pool, path,
                                             strlen(path) - strlen(suffix) - 1);
             }

commit 47d98e77b2b90504c284ac130516dfef551c5228

Resolve a regression in the reintegrate path calculations introduced by optimizations made in r1311669.
* subversion/libsvn_client/merge.c
  (log_find_operative_revs): Don't corrupt mergeinfo paths by snipping off
   trailing characters. This function is trying to compute a common prefix
   of paths by snipping off a suffix detected with relpath_skip_ancestor().
   If relpath_skip_ancestor() returns "" it wants to tell its caller that
   the paths provided were equal. This function erroneously went on to
   perform path arithmetic anyway, with a zero-sized suffix string, and
   thus computed corrupt mergeinfo.

Reported by: Andrew Reedick
  http://svn.haxx.se/users/archive-2013-09/0116.shtml
  http://svn.haxx.se/users/archive-2013-09/0138.shtml
Review by: rhuijben


git-svn-id: http://svn.apache.org/repos/asf/subversion/trunk@1523666 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/merge.c
@@ -10715,7 +10715,7 @@ log_find_operative_revs(void *baton,
 
           suffix = svn_relpath_skip_ancestor(subtree_missing_this_rev,
                                              source_rel_path);
-          if (suffix)
+          if (suffix && suffix[0] != '\0')
             {
               missing_path = apr_pstrmemdup(pool, path,
                                             strlen(path) - strlen(suffix) - 1);